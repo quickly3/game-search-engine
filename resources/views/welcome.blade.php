bce62c0b34bb9dddf447bdbacbc4bc8e9a941286
HongBin Zhou <quickly3@sohu.com>
2018-06-07 17:06:54 +0800

    First add

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+
+    </head>
+    <body>
+        <app-root></app-root>
+        <script type="text/javascript" src="dist/angular/runtime.js"></script>
+        <script type="text/javascript" src="dist/angular/polyfills.js"></script>
+        <script type="text/javascript" src="dist/angular/styles.js"></script>
+        <script type="text/javascript" src="dist/angular/vendor.js"></script>
+        <script type="text/javascript" src="dist/angular/main.js"></script>
+    </body>
+</html>
---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: '../assets/html/app.component.html',
+  styleUrls: ['../assets/scss/app.component.scss']
+})
+export class AppComponent {
+  title = 'app';
+}
